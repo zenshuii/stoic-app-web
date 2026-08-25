aec68aa03dc72d8076de160610456213315503a2
SMelidoni <simone.melidoni@gmail.com>
2026-08-25 21:29:06 +0100

    refactor: make Stoic preview icons exhaustive

--- src/components/StoicAppPreview.tsx
+++ src/components/StoicAppPreview.tsx
@@ -108,11 +108,16 @@ function StoicPreviewIcon({
     )
   }
 
-  return (
-    <svg {...iconProps}>
-      <path d="m41 45-17-10L7 45V6a4 4 0 0 1 4-4h26a4 4 0 0 1 4 4z" />
-    </svg>
-  )
+  if (name === 'bookmark') {
+    return (
+      <svg {...iconProps}>
+        <path d="m41 45-17-10L7 45V6a4 4 0 0 1 4-4h26a4 4 0 0 1 4 4z" />
+      </svg>
+    )
+  }
+
+  const unsupportedName: never = name
+  throw new Error(`Unsupported Stoic preview icon: ${unsupportedName}`)
 }
 
 export function StoicAppPreview() {
