279f25f19a8dda1b1fc35d4479385173ff835cc3
SMelidoni <simone.melidoni@gmail.com>
2026-08-25 22:20:45 +0100

    fix: reveal content when animation loading fails

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,28 @@ function App() {
     if (prefersReducedMotion.matches) return
 
     let cancelled = false
-    const animationFrame = window.requestAnimationFrame(() => {
-      void import('aos').then(({ default: AOS }) => {
-        if (cancelled) return
-
-        AOS.init({
-          duration: 750,
-          once: true,
-          easing: 'ease-out-cubic',
-          offset: 80,
-        })
+    const revealAnimatedContent = () => {
+      document.querySelectorAll('[data-aos]').forEach((element) => {
+        element.removeAttribute('data-aos')
+        element.removeAttribute('data-aos-delay')
       })
+    }
+
+    const animationFrame = window.requestAnimationFrame(() => {
+      void import('aos')
+        .then(({ default: AOS }) => {
+          if (cancelled) return
+
+          AOS.init({
+            duration: 750,
+            once: true,
+            easing: 'ease-out-cubic',
+            offset: 80,
+          })
+        })
+        .catch(() => {
+          if (!cancelled) revealAnimatedContent()
+        })
     })
 
     return () => {
